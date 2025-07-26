33c575d90f6df5cf40b2f2573cc866394114c419
AliSolaxay <alibavaria1860@gmail.com>
2025-07-26 13:10:02 +0330

    SPT.py

---
+++ SPT.py
@@ -0,0 +1,14 @@
+"""
+A program to add two numbers
+"""
+
+
+
+first_number = 5
+second_number = 8
+if first_number != 0:
+    print(f"a+b = {first_number+second_number}")
+
+
+else:
+    print("a = 0!")
